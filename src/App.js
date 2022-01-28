1018653e58cb75354d5a587f5cf2ecfc3de5455a
emorilebo <emorylebo@gmail.com>
2022-01-28 11:36:28 +0100

    ADD currency Options

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import CurrencyRow from "./CurrencyRow";
 
@@ -6,18 +6,23 @@ const BASE_URL =
   "http://api.exchangeratesapi.io/v1/latest?access_key=a16fbb7a7037ce68996401d86f90b197";
 
 export default function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  console.log(currencyOptions);
+
   useEffect(() => {
     fetch(BASE_URL)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+      });
   }, []);
   return (
     <div>
       <h1>Currency Converter</h1>
       <div className="flex">
-        <CurrencyRow title={"From"} />
+        <CurrencyRow title={"From"} currencyOptions={currencyOptions} />
         <div className="equals">=</div>
-        <CurrencyRow title={"To"} />
+        <CurrencyRow title={"To"} currencyOptions={currencyOptions} />
       </div>
     </div>
   );
